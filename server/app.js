5350cf2ad0ed4456333a2048038ae2407a089b53
cwklausing <cwklausing@gmail.com>
2016-01-10 16:59:50 -0600

    Begin angular branch

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ var index = require('./routes/index');
 //App Settings
 app.set("port", process.env.PORT || 5000);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({expanded: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //DB Stuff
